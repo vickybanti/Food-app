169709110f524df148fb70c6b3204b0f0076d02c
VICTOR <olamuyiwavictor55@gmail.com>
2024-11-01 22:14:01 +0000

    added color to categorytype

--- src/lib/utils/auth.ts
+++ src/lib/utils/auth.ts
@@ -7,7 +7,7 @@ import User from "../database/models/user.model";
 
 declare module "next-auth"{
   interface Session{
-    user:User & {
+    user:typeof User & {
       isAdmin:Boolean;
     }
   }
