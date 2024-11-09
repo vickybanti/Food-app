02b058bb22bc7ff86b83750ba9d2821ed1bfcaf3
VICTOR <olamuyiwavictor55@gmail.com>
2024-11-09 22:15:07 +0100

    responsiveness

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -41,7 +41,7 @@ const Slider = () => {
   }, []);
 
   return (
-    <div className="heroSlideImg  mr-10 flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-[#fcf6ef] transition-transform duration-500 ease-in-out mx-35 my-36">
+    <div className="mr-10 flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-[#fcf6ef] transition-transform duration-500 ease-in-out mx-35 my-36">
       {/* TEXT CONTAINER */}
       <div
         className={`flex-1 flex items-center justify-center flex-col gap-15 font-bold transition-all duration-500 ease-in-out mx-10 ${
