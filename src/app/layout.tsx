9e0a7134d68b18475c8d729140b8d90d259cec9d
VICTOR <olamuyiwavictor55@gmail.com>
2024-11-09 16:21:32 +0100

    responsivenes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`bg-[#fcf6ef] sm:overflow-x-hidden md:p-2`}>
+      <body className={`bg-[#fcf6ef] mobile`}>
         <AuthProvider>
           <QueryProvider>
         <div className="relative">
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -99,7 +99,7 @@ const Featured = () => {
   }, []);
 
   return (
-    <section className='relative flex flex-col w-full h-full py-5 overflow-x-auto no-scrollbar 2xl:max-container lg:mb-10 lg:py-20 xl:mb-20 mt-14'>
+    <section className='relative flex flex-col w-full h-full py-5 overflow-x-auto feature no-scrollbar 2xl:max-container lg:mb-10 lg:py-20 xl:mb-20 mt-14'>
 
 <div className="flex justify-between mx-20 mb-5" >
 <h2 className="font-sans text-3xl font-semibold text-gray-900 ">Featured Products</h2>
@@ -108,7 +108,7 @@ const Featured = () => {
 </div>
 
 
-<div className='flex items-start justify-start w-full h-full gap-8 px-2 mx-20 mt-10 overflow-x-auto no-scrollbar'>  
+<div className='flex items-start justify-start w-full h-full gap-8 px-2 mx-20 mt-10 overflow-x-auto no-scrollbar feature'>  
     {/* WRAPPER */}
       <div className="relative flex mt-10 w-max">
         {/* SINGLE ITEM */}
