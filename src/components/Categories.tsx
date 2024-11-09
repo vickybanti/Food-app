790e530e3bcdc1368bea1fe76e487eb86af0d6a2
VICTOR <olamuyiwavictor55@gmail.com>
2024-11-09 12:48:18 +0100

    responsivenes

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -42,35 +42,38 @@ const Categories = () => {
   
   
   return (
-    <div className="md py-7 mx-20 border-t-2 border-t-[#B78C56] mt-4 md:mx-20">
+    <><div className='mx-20 border-t-2 border-t-[#B78C56] '>
       <h2 className="mb-4 font-sans text-3xl font-semibold text-gray-900 ">Categories</h2>
-      <Carousel>
-        <CarouselContent className='mdImg'>
-          {allCategories.map((category) => (
-            
-            <CarouselItem key={category._id} className="lg:basis-1/3">
-
-              <div
-                className={`p-4 rounded-sm bg-${category.color}-100 relative overflow-hidden group w-full h-[300px] cursor-pointer`}
-                onClick={() => router.push(`/products?category=${category.slug}`)}
-              >
-                <Image
-                  src={category.img || ''}
-                  alt={category.title}
-                  fill
-                  className='object-cover w-full h-full transition-all duration-300 slideImg' />
-                <div className={`absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 transform translate-y-full transition-transform duration-300 group-hover:translate-y-0 h-screen flex items-center justify-center `}>
-                  <p className={`font-semibold  text-[50px] font-sans pt-72`}>{category.title}</p>
-                </div>
-              </div>
-
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
     </div>
+    <div className="mx-20 mt-4 md py-7 md:mx-20">
+
+        <Carousel>
+          <CarouselContent className='mdImg'>
+            {allCategories.map((category) => (
+
+              <CarouselItem key={category._id} className="lg:basis-1/3">
+
+                <div
+                  className={`p-4 rounded-sm bg-${category.color}-100 relative overflow-hidden group w-full h-[300px] cursor-pointer`}
+                  onClick={() => router.push(`/products?category=${category.slug}`)}
+                >
+                  <Image
+                    src={category.img || ''}
+                    alt={category.title}
+                    fill
+                    className='object-cover w-full h-full transition-all duration-300 slideImg' />
+                  <div className={`absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 text-white p-2 transform translate-y-full transition-transform duration-300 group-hover:translate-y-0 h-screen flex items-center justify-center `}>
+                    <p className={`font-semibold  text-[50px] font-sans pt-72`}>{category.title}</p>
+                  </div>
+                </div>
+
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div></>
   )
 }
 
