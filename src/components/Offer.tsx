07998ba3847dc80faa9fecfc7632d31d50b36513
VICTOR <olamuyiwavictor55@gmail.com>
2024-11-10 22:48:45 +0100

    responsiveness

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -7,9 +7,9 @@ import Button from "./Button";
 const Offer = () => {
   return (
     <div className="pt-32 mt-0 bg-fuchsia-50 offer">
-    <div className="bg-black offer-bg h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] mx-20">
+    <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] mx-20">
       {/* TEXT CONTAINER */}
-      <div className="flex flex-col items-center justify-center flex-1 gap-8 p-6 text-center">
+      <div className="offer-bg flex flex-col items-center justify-center flex-1 gap-8 p-6 text-center">
         <h1 className="text-5xl font-bold text-white xl:text-6xl">Delicious Burger & French Fry</h1>
         <p className="text-white xl:text-xl">
           Progressively simplify effective e-toilers and process-centric methods
