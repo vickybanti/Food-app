850fb1b56b356667e0e9e8cd2ac087f62c764b86
VICTOR <olamuyiwavictor55@gmail.com>
2024-12-30 11:13:25 +0100

    responsiveness

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -9,9 +9,9 @@ const Offer = () => {
   const router = useRouter()
   const dates = new Date();
   return (
-    <div className="lg:flex md:flex md:overflox-x-auto justify-between lg:w-full md:w-[40%] gap-2 pt-10 mt-0">
-      <div className="w-[100%] rounded-lg h-80 mx-6">
-    <div className="bg-black h-full flex flex-col md:flex-col md:gap-2  md:bg-[url('/offerBg.png')] mx-5 rounded-2xl">
+    <div className="lg:flex md:flex md:overflow-x-auto justify-between lg:w-full md:w-[40%] gap-2 pt-10 mt-0">
+      <div className="lg:w-[100%] md:w-[20%] rounded-lg lg:h-80 md:h-32 lg:mx-6">
+    <div className="bg-black h-full flex flex-col md:flex-col md:gap-2  md:bg-[url('/offerBg.png')] lg:mx-5 rounded-2xl">
       {/* TEXT CONTAINER */}
       <div className="flex lg:flex-col md:flex md:flex-col items-center justify-center flex-1 gap-8 p-6 text-center md:w-[20%] lg:w-full ">
         <h1 className="lg:text-xl md:text-sm font-bold text-white">Delicious Burger & French Fry</h1>
@@ -50,7 +50,7 @@ const Offer = () => {
     </div>
 
 
-    <div className="lg:w-[100%] md:w-[30%] rounded-lg lg:h-80 md:h-32 mx-6">
+    <div className="lg:w-[100%] md:w-[50%] rounded-lg lg:h-80 md:h-32 mx-6">
     <div className="h-full mx-5 rounded-2xl">
       {/* TEXT CONTAINER */}
       
